d4e8cdb71c3ed54030f0bc721d95290f4c657ea3
shadow061103 <shadow061103@gmail.com>
2018-07-24 11:29:14 +0800

    generic enum to attribute

--- Model/AttributeHelper.cs
+++ Model/AttributeHelper.cs
@@ -9,26 +9,28 @@ namespace Model
 {
    public static class AttributeHelper//擴充方法
     {
+        public static K GetAttribute<T, K>(T enumtype) where K :Attribute where T:struct
+        {
+            FieldInfo data = typeof(T).GetField(enumtype.ToString());
+            Attribute attr = Attribute.GetCustomAttribute(data, typeof(K));
+            K strategy = (K)attr;
+            return strategy;
+        }
+
         public static Type GetStrategyType(this GenderType gender)
         {
-            FieldInfo data = typeof(GenderType).GetField(gender.ToString());
-            Attribute attr = Attribute.GetCustomAttribute(data, typeof(StrategyAttribute));
-            StrategyAttribute strategy = (StrategyAttribute)attr;
-            return strategy.StrategyType;
+            return GetAttribute<GenderType, StrategyAttribute>(gender).StrategyType;
+           
         }
         public static double GetActivityType(this ActivityType activity)
         {
-            FieldInfo data = typeof(ActivityType).GetField(activity.ToString());
-            Attribute attr = Attribute.GetCustomAttribute(data, typeof(ActivityAttribute));
-            ActivityAttribute Actmult = attr as ActivityAttribute;
-            return Actmult.Activitymulti;
+            return GetAttribute<ActivityType, ActivityAttribute>(activity).Activitymulti;
+            
         }
         public static double GetTdeePlus(this Goal goal)
         {
-            FieldInfo data = typeof(Goal).GetField(goal.ToString());
-            Attribute attr = Attribute.GetCustomAttribute(data, typeof(GoalAttribute));
-            GoalAttribute tdeeplus = attr as GoalAttribute;
-            return tdeeplus.TdeePlus;
+            return GetAttribute<Goal, GoalAttribute>(goal).TdeePlus;
+           
         }
     }
 }
